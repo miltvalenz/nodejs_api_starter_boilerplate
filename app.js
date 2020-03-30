9b7c048227da2f0a6c5b7eb7b2c164693e95a0e5
unknown <miltvalenz@hotmail.com>
2020-03-30 19:54:09 -0400

    Initialize cors and express-session

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require('dotenv');
+const passport = require('passport');
+const session = require('express-session');
+const cors = require('cors');
 
 const api = require('./src/api');
 const app = express();
@@ -12,6 +15,7 @@ const app = express();
  */
 dotenv.config();
 require('./src/db/database');
+require('./src/middleware');
 
 /**
  * Middlewares
@@ -20,6 +24,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+	session({
+		secret: 'mysecretapp',
+		resave: true,
+		saveUninitialized: true
+	})
+);
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 /**
