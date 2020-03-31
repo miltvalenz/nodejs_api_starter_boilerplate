e68e884cd5f7ec42876045440339ef4edbd79b09
unknown <miltvalenz@hotmail.com>
2020-03-31 02:22:03 -0400

    Authenticate get users route

--- src/routes/users/getUsers.js
+++ src/routes/users/getUsers.js
@@ -2,14 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
-module.exports = ({ User }) => {
-	router.get("/", async (req, res, next) => {
-		try {
-			const users = await User.find({});
-			res.status(200).json({ Users: users });
-		} catch {
-			next(error);
-		}
+module.exports = ({ User }, passport) => {
+	router.get(
+		'/', 
+		passport.authenticate('jwt', { session: false }), 
+		async (req, res, next) => {
+			try {
+				const users = await User.find({});
+				res.status(200).json({ Users: users });
+			} catch {
+				next(error);
+			}
 	});
 
   return router;
